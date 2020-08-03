54d4aaff162ccd6034b51daf9a1c53307c1fefc7
mhssvkp <mhssvkp@gmail.com>
2020-08-03 12:23:27 +0530

    made changes to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment";
 import { Recipe } from "./models/recipe";
-import LocalStorageService from "./services/localstorage-service";
 import { SearchService } from "./services/search-service/search.service";
 import { Router } from "@angular/router";
 
@@ -16,23 +13,10 @@ export class AppComponent {
   recipes: Array<Recipe> = new Array();
   searchText: string = "";
 
-  constructor(
-    private httpClient: HttpClient,
-    private localStorageService: LocalStorageService,
-    private searchService: SearchService,
-    private router: Router
-  ) {
-    httpClient
-      .get(environment.recepeDetailsUrl)
-      .subscribe((response: Array<Recipe>) => {
-        this.recipes = response;
-        localStorageService.storeRecipes(this.recipes);
-      });
-  }
+  constructor(private searchService: SearchService, private router: Router) {}
 
   search() {
     console.log(this.searchText);
-
     this.searchService.searchTerm.next(this.searchText);
     this.router.navigate(["home"]);
   }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import LocalStorageService from "src/app/services/localstorage-service";
 import { Recipe } from "src/app/models/recipe";
 import { SearchService } from "src/app/services/search-service/search.service";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-home",
@@ -13,12 +15,17 @@ export class HomeComponent implements OnInit {
   filteredRecipes: Array<Recipe> = new Array();
   constructor(
     private localStorageService: LocalStorageService,
-    private searchService: SearchService
+    private searchService: SearchService,
+    private httpClient: HttpClient
   ) {}
 
   ngOnInit(): void {
-    this.filteredRecipes = this.recipes = this.localStorageService.fetchRecipes();
-
+    this.httpClient
+      .get(environment.recepeDetailsUrl)
+      .subscribe((response: Array<Recipe>) => {
+        this.filteredRecipes = this.recipes = response;
+        this.localStorageService.storeRecipes(this.recipes);
+      });
     this.searchService.searchTerm.subscribe((val) => {
       if (val) {
         this.filteredRecipes = this.recipes.filter((recipe) =>
